7b93c616004534a93342e90ea4186027ef1020aa
kamen.ivanov <kamen.ivanov@egt-digital.com>
2026-06-26 15:56:48 +0300

    Renamed groupid

--- dao-api/src/main/java/com/pe/multimodule/dao/api/Main.java
+++ dao-api/src/main/java/com/pe/multimodule/dao/api/Main.java
@@ -1,4 +1,4 @@
-package com.pe.spring.boot;
+package com.pe.multimodule.dao.api;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
--- domain/src/main/java/com/pe/multimodule/domain/Main.java
+++ domain/src/main/java/com/pe/multimodule/domain/Main.java
@@ -1,4 +1,4 @@
-package com.pe.spring.boot;
+package com.pe.multimodule.domain;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
